1ab11b5d4537e0adfa026199c85a5d90ab921df4
Sean Morrison & Justin Speers <student@epicodus.com>
2014-08-13 15:07:32 -0700

    Add spec for listing all projects for all employees in a division.

--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -9,4 +9,23 @@ describe 'Division' do
     test_division.employees << test_employee2
     expect(test_division.employees).to eq [test_employee1, test_employee2]
   end
+
+  it "can list all projects for all employees in a division" do
+    employee1 = Employee.create({:name => "slacker"})
+    project1 = Project.create({:name => "employee tracker"})
+    division1 = Division.create({:name => "whatever"})
+    contributions = [ Contribution.create({:description => "did stuff"}),
+                      Contribution.create({:description => "didn't do stuff"})]
+
+    contributions.each do |contribution|
+      project1.contributions << contribution
+      employee1.contributions << contribution
+    end
+    division1.employees << employee1
+
+    expect(project1.employees.first).to eq employee1
+    expect(division1.employees[0].projects[0]).to eq project1
+  end
 end
+
+
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 
 describe "Project" do
   it "has many employees tied to it through contributions" do
-    employee = Employee.create({:name => "slacker"})
-    project = Project.create({:name => "employee tracker"})
+    employee1 = Employee.create({:name => "slacker"})
+    project1 = Project.create({:name => "employee tracker"})
+    division1 = Division.create({:name => "whatever"})
     contributions = [ Contribution.create({:description => "did stuff"}),
                       Contribution.create({:description => "didn't do stuff"})]
 
     contributions.each do |contribution|
-      project.contributions << contribution
-      employee.contributions << contribution
+      project1.contributions << contribution
+      employee1.contributions << contribution
     end
-    expect(project.employees.first).to eq employee
+    expect(project1.employees.first).to eq employee1
   end
 end
 
